af791cf51a0f5ccce3a40fa9e064f1c077ba800b
vibster <vaibhavsrivastava1599@gmail.com>
2021-05-13 16:35:29 +0530

    database connected to online

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const url = 'mongodb+srv://vibsterr:vibsterr99@cluster0.g1pdc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const url = 'mongodb+srv://vibsterr:vibsterr99@cluster0.g1pdc.mongodb.net/QkRes?retryWrites=true&w=majority'
 const app=express()
 
 mongoose.connect(url,{useNewUrlParser:true})
@@ -15,10 +15,10 @@ app.use(express.json())
 
 
 const seatsrouter = require('./routes/seats')
-app.use('/seats',seatsrouter)
+app.use('/seat',seatsrouter)
 const docrouter = require('./routes/doctors')
 app.use('/doctor',docrouter)
 
-app.listen(9000,function(){
+app.listen(1000,function(){
     console.log('Server Started')
 })
